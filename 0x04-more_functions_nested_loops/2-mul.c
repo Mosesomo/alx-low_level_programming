8e5294687ccfbe8a2c44711832e9379ef1178991
Mosesomo <moseswamboga4@gmail.com>
2023-03-24 05:24:41 -0700

    function to multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *mul - function to multiply two numbers
+ *
+ *@a: function parameter to be multiplied
+ *@b: function parameter to be multiplied
+ *Return: always 0
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
