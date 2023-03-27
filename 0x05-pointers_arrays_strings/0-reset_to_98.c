9f7f2f6dee05f509d949123e237d0e4ae60241ea
Mosesomo <moseswamboga4@gmail.com>
2023-03-27 08:39:22 -0700

    function that updates a value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that updates a value
+ *
+ * @n: function parameter to be updated
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
