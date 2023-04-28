a471387ca99ffbbfe38e89f98bae33ea723ebcbc
Mosesomo <moseswamboga4@gmail.com>
2023-04-28 06:19:00 -0700

    prints before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *func - prints a sentence before the main
+ *functions is excuted
+ */
+
+void func(void)
+{
+	printf("You're beat! and yet, you must allow, \n");
+	printf("I bore my house my back!\n");
+}
+
