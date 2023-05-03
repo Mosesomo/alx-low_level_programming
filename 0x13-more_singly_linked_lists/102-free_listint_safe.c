8d579ad4c34aeacada5bafff6cbfbc3d5989365f
Mosesomo <moseswamboga4@gmail.com>
2023-05-03 05:31:35 -0700

    free safe

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,6 +1,52 @@
+#include <stdio.h>
 #include "lists.h"
 #include <stdlib.h>
-#include <stddef.h>
+
+size_t looped_listint_len2(const listint_t *head);
+size_t  free_listint_safe(listint_t **h);
+
+/**
+ *looped_listint_len2 - counts the number of uniique nodes
+ *@head: pointer to the first node
+ *Return: if the list is not looped return 0
+ *otherwise return number of nodes
+ */
+
+size_t looped_listint_len2(const listint_t *head)
+{
+	const listint_t *tortoise, *hare;
+	size_t nodes = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+	tortoise = head->next;
+	hare = (head->next)->next;
+
+	while (hare)
+	{
+		if (tortoise == hare)
+		{
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+			tortoise = tortoise->next;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+			}
+			return (nodes);
+		}
+		tortoise = tortoise->next;
+		hare = (hare->next)->next;
+	}
+	return (0);
+}
+
 
 /**
  *free_listint_safe - frees list
