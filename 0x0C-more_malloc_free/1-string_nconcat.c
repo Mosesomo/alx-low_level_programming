b05ce199fdfb04231bde9fad4f392130c65df13f
Mosesomo <moseswamboga4@gmail.com>
2023-06-15 06:05:26 -0700

    nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,11 +14,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *ncont;
 	unsigned int x = 0, y = 0, len1 = 0, len2 = 0;
 
-	if (s1 == NULL && s2 == NULL)
-	{
+	if (s1 == NULL)
 		s1 = "";
+	if (s2 ==  NULL)
 		s2 = "";
-	}
 
 	while (s1[len1] != '\0')
 		len1++;
@@ -46,6 +45,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (y < n)
 	{
 		ncont[x + y] = s2[y];
+		x++;
 		y++;
 	}
 	ncont[x + y] = '\0';
