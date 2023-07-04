4a3b895c38fcecd52f15ab87188731caeda96e9c
Mosesomo <moseswamboga4@gmail.com>
2023-07-04 03:00:16 -0700

    insrt node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 
 	newnode = malloc(sizeof(listint_t));
-	if (newnode == NULL || *head == NULL)
+	if (newnode == NULL || head == NULL)
 		return (NULL);
 	newnode->n = n;
 	newnode->next = NULL;
