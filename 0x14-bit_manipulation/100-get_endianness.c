b29b02352fb561588079f02cceeffbc7cbb8cbee
Mosesomo <moseswamboga4@gmail.com>
2023-07-06 23:47:14 -0700

    endiannes

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *get_endianness - checks for endiannes in the machine
+ *Return: 1 if little and 0 if big
+ */
+
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *ptr = (char *) & i;
+
+	return (*ptr);
+}
