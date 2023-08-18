e2247ead2f4b6e03cb9fda6fa89a44416795f64c
Mosesomo <moseswamboga4@gmail.com>
2023-08-18 06:32:55 -0700

    delete node

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -34,6 +34,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			if (temp2->prev != NULL)
 				temp2->prev->next = temp2->next;
 			free(temp2);
+			return (1);
 		}
 		temp2 = temp2->next;
 		count++;
