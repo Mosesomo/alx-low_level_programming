ab1e5a3fe86bf7160814fa791ff2f60b2e2a1779
Mosesomo <moseswamboga4@gmail.com>
2023-08-21 15:28:00 -0700

    Let's call C functions from Python

---
+++ 0x18-dynamic_libraries/oprations.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+/**
+ *add - function that retirn sum of two numbers
+ *@a: inpu
+ *@b: input
+ *Return: sum
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ *sub - returns the substraction of two numbers
+ *@a: input
+ *@b: input
+ *Return: substraction
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *div - returns the division of two numbers
+ *@a: input
+ *@b: input
+ *Return: always 0 on success
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ *mul - return the product of two numbers
+ *@a: input
+ *@b: input
+ *Return: always 0 on success
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ *mod - returns the remainder
+ *@a: input
+ *@b: input
+ *Return: always 0 on success
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
