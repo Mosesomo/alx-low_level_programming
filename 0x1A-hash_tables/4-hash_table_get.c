76908372980900799769245b8e8ba7214c654720
Mosesomo <moseswamboga4@gmail.com>
2023-09-07 13:35:20 -0700

    retrieve value

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ *hash_node_get - retrieves value associated with the key
+ *@ht: hash table to look
+ *@key: key that is being looked for
+ *Return: value associated with the key
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *current;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
+
+	current = ht->array[index];
+	while (current)
+	{
+		if (strcmp(current->key, key) == 0)
+			return (current->value);
+		current = current->next;
+	}
+	if (current == NULL)
+		return (NULL);
+	return (current->value);
+}
+		
+
