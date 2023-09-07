d27e9f1740182015b618a6285ad98f7f7e1acdd6
Mosesomo <moseswamboga4@gmail.com>
2023-09-07 10:30:41 -0700

    add element to a node

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -45,8 +45,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 
 	new->value = dup_value;
-	new->next = ht->array[index];
-	ht->array[index] = new;
+	new->next = curr;
+	curr = new;
 
 	return (1);
 }
